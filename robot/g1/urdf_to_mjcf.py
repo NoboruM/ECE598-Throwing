76fb1ebd0b1a6e21f423d4e97bacf47821019571
noborum <noborum2@illinois.edu>
2025-04-15 03:55:21 -0500

    added model with psyonic hands, but controls not setup yet

--- robot/g1/urdf_to_mjcf.py
+++ robot/g1/urdf_to_mjcf.py
@@ -1,9 +1,9 @@
 import mujoco
 
 # Load the URDF file
-model = mujoco.MjModel.from_xml_path('/home/noboru/KIMLAB_WS/G1_ws/src/unitree_mujoco/unitree_robots/g1/ability_hand_right_small.urdf')
+model = mujoco.MjModel.from_xml_path('/home/noboru/KIMLAB_WS/G1_ws/src/ECE598-Throwing/robot/g1/ability_hand_left.urdf', )
 
 # Save as MJCF
-mujoco.mj_saveLastXML('/home/noboru/KIMLAB_WS/G1_ws/src/unitree_mujoco/unitree_robots/g1/ability_hand_right_small.xml', model)
+mujoco.mj_saveLastXML('/home/noboru/KIMLAB_WS/G1_ws/src/ECE598-Throwing/robot/g1/ability_hand_left.xml', model)
 
 print("Conversion complete. MJCF file saved.")
