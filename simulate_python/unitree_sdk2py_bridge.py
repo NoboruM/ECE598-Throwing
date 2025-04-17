668631330d05ca744c94b75d84e327d52b6e9533
noborum <noborum2@illinois.edu>
2025-04-17 13:39:21 -0500

    modified stiffness to closely match G1's mass

--- simulate_python/unitree_sdk2py_bridge.py
+++ simulate_python/unitree_sdk2py_bridge.py
@@ -399,7 +399,7 @@ class UnitreeSdk2Bridge:
 class ElasticBand:
 
     def __init__(self):
-        self.stiffness = 200
+        self.stiffness = 150
         self.damping = 100
         self.point = np.array([0, 0, 3])
         self.length = 0
